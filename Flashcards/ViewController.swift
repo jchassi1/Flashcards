b14954a0b740774c68be5c5648c6b33033613510
Justin Chassin <justinchassin@justins-macbook-pro.local>
2018-10-13 17:25:29 -0400

    Simple flashcards app done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden=true
+    }
+    
+    
 }
 
